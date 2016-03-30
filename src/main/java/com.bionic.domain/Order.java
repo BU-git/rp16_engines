78d866ea6d956b5101b4b50d1c00c501cd8d7d26
roman_kutsenko <fantom_9119@mail.ru>
2016-03-30 18:34:21 +0300

    add .gitignore

--- src/main/java/com.bionic.domain/Order.java
+++ src/main/java/com.bionic.domain/Order.java
@@ -1,4 +1,27 @@
 package com.bionic.domain;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+//@Table(name = "ORDERS")
+@SequenceGenerator(name = "sequence", sequenceName = "ORDER_SEQ", allocationSize = 1, initialValue = 1)
 public class Order {
+
+    @Id
+    @Column(name = "ORDER_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "sequence")
+    private Long id;
+
+    @Column(name = "ORDER_NUMBER")
+    private Integer orderNumber;
+
+    @Column(name = "SERVICE_DATE")
+    private Date serviceDate;
+
+    @Column(name = "ORDER_TASK")
+    private String task;
+
+    @Column(name = "ORDER_ADDRESS")
+    private String address;
 }
