eaaaac8b0c7922163c210235bbb1a326204df2ed
roman_kutsenko <fantom_9119@mail.ru>
2016-04-06 16:02:52 +0300

    Create dasboard functionality, session-attribute, some changes in controller

--- src/main/java/com/bionic/controller/LoginController.java
+++ src/main/java/com/bionic/controller/LoginController.java
@@ -1,13 +1,11 @@
 package com.bionic.controller;
 
 import javax.inject.Inject;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import com.bionic.domain.User;
 import com.bionic.service.UserService;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -22,12 +20,15 @@ public class LoginController {
 	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String displayLogin(ModelMap model) {
+		if (model.containsAttribute("loggedInUser")) {
+			return "dashboard";
+		}
 		model.addAttribute("user", new User());
 		return "login";
 	}
 	
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String login(@ModelAttribute("user")User user, ModelMap model) {
+	@RequestMapping(value = "/dashboard", method = RequestMethod.POST)
+	public String authorization(@ModelAttribute("user")User user, ModelMap model) {
 		String message = null;
 		boolean isValidUser = userService.isValidUser(user.getMail(), user.getPassword());
 		if (isValidUser) {
@@ -42,15 +43,25 @@ public class LoginController {
 
     @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
     public String dashboardPage(ModelMap model) {
-        /*if (!model.containsAttribute("loggedInUser")) {
-            return "login";
-        }*/
+        if (!model.containsAttribute("loggedInUser")) {
+			//model.addAttribute("message", "You need to be logged!");
+            return "redirect:login.html";
+        }
         return "dashboard";
     }
 
     @RequestMapping("/logout")
     public String logOut (SessionStatus sessionStatus){
         sessionStatus.setComplete();
-        return "login";
+        return "redirect:login.html";
     }
+
+	@RequestMapping(value = "/templates", method = RequestMethod.GET)
+	public String templatesPage(ModelMap model) {
+		if (!model.containsAttribute("loggedInUser")) {
+			//model.addAttribute("message", "You need to be logged!");
+			return "redirect:login.html";
+		}
+		return "templates";
+	}
 }
