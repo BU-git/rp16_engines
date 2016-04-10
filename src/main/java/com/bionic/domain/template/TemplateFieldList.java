db87c796ab2a9225a26163add46d5c3eecaa03fa
ffwATL <4else@i.ua>
2016-04-10 20:50:24 +0300

    added template name field

--- src/main/java/com/bionic/domain/template/TemplateFieldList.java
+++ src/main/java/com/bionic/domain/template/TemplateFieldList.java
@@ -6,10 +6,21 @@ import java.util.List;
 public class TemplateFieldList {
 
     private List<TemplateField> fields;
+    private String templateName;
+
+    public String getTemplateName() {
+        return templateName;
+    }
+
+    public void setTemplateName(String templateName) {
+        this.templateName = templateName;
+    }
 
     public List<TemplateField> getFields() {
         return fields;
     }
+
+
 
     public void setFields(List<TemplateField> fields) {
         this.fields = fields;
