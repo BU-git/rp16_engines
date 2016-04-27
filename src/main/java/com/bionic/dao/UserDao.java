68fbfb2dff66485738111f8c5a734859c2fe7fc1
ffwATL <4else@i.ua>
2016-04-27 22:44:59 +0300

    role added

--- src/main/java/com/bionic/dao/UserDao.java
+++ src/main/java/com/bionic/dao/UserDao.java
@@ -1,11 +1,11 @@
 package com.bionic.dao;
 
-import com.bionic.domain.User;
-
 import java.util.List;
 
+import com.bionic.domain.User;
+
 public interface UserDao {
-	List<User> getUser(String mail, String password);
+	List<User> adminLogin(String mail, String password);
 	List<User> getAllUsers();
 	void save(User u);
 	List<User> findByEmail(String email);
