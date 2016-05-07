e4d71b04742bdde499ab86f5ca761cabab61ba32
roman_kutsenko <fantom_9119@mail.ru>
2016-05-07 14:33:59 +0300

    [EN2016-135] update

--- src/main/java/com/bionic/controller/OrderUploadController.java
+++ src/main/java/com/bionic/controller/OrderUploadController.java
@@ -8,7 +8,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
 import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServletRequest;
 import java.io.*;
 import java.util.Date;
 
@@ -37,14 +40,54 @@ public class OrderUploadController {
         return new ResponseEntity(HttpStatus.OK);
     }
 
-
-    @RequestMapping(value = "upload/{number}", method = RequestMethod.POST, /*headers=("content-type=multipart*//*"),*/ consumes = "multipart/form-data")
+/*
+    @RequestMapping(value = "upload/{number}", method = RequestMethod.POST, consumes = "multipart/form-data")
     public ResponseEntity uploadFile(@PathVariable("number") long number,
                                      //@RequestParam("checksum") MultipartFile checksum,
                                      @RequestBody MultipartFile fileData) {
-        System.out.println(fileData.getSize() + " =  SIZE");
-        System.out.println(fileData.getOriginalFilename() + " = NAME");
-        System.out.println();
+        System.out.println("upload start!!!!!");
+        try {
+            if (fileData.isEmpty() || fileData == null) {
+                System.out.println("null!!!!!!!!!!!!!!!111");
+                return new ResponseEntity(HttpStatus.BAD_REQUEST);
+            }
+            System.out.println();
+            File file = null;
+            if (!fileData.isEmpty()) {
+                try {
+                    file = new File(ROOT + "/" + number + ".pdf");
+                    BufferedOutputStream stream = new BufferedOutputStream(
+                            new FileOutputStream(file));
+                    FileCopyUtils.copy(fileData.getInputStream(), stream);
+                    stream.close();
+                    System.out.println("You successfully uploaded " + fileData.getName() + "!");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+            } else {
+                return new ResponseEntity(HttpStatus.BAD_REQUEST);
+            }
+            String link = file.getAbsolutePath();
+            Order order = orderService.findById(number);
+            order.setPdfLink(link);
+            orderService.save(order);
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }*/
+
+    @RequestMapping(value = "upload/{number}", method = RequestMethod.POST, headers=("content-type=multipart*//*"), consumes = "multipart/form-data")
+    public ResponseEntity uploadFile(@PathVariable("number") long number, MultipartHttpServletRequest request) {
+        System.out.println("Starting upload method...");
+        MultipartFile fileData = request.getFile("file");
+        /*if (fileData == null || fileData.isEmpty()) {
+            System.out.println("File is empty");
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }*/
+
         File file = null;
         if (!fileData.isEmpty()) {
             try {
