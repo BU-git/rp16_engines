6e71aa160f7dad8ef7bcf630e9e40127fe4d560f
roman_kutsenko <fantom_9119@mail.ru>
2016-05-13 20:10:08 +0300

    new DB

--- src/main/java/com/bionic/service/UserServiceImpl.java
+++ src/main/java/com/bionic/service/UserServiceImpl.java
@@ -4,10 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
