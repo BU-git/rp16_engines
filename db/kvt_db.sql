5977bcc4e7ef1994b160b3505224f19e4ac40d54
roman_kutsenko <fantom_9119@mail.ru>
2016-05-25 08:42:39 +0300

    new db

--- db/kvt_db.sql
+++ db/kvt_db.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `kvt_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `kvt_db`;
 -- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: kvt_db
@@ -470,4 +472,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-05-25  8:30:57
+-- Dump completed on 2016-05-25  8:40:22
